009577f14efe1a3736343634d67f2ecd04eca97e
TRAVELJIGSAW\lowndesa <alexlowndes@rentalcars.com>
2019-01-25 16:35:07 +0000

    commenting the outline of what's needed

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
 console.log("Hooray");
+
+// Name - Look into faker.js
+
+// Race
+
+// Class
+
+// Specialization
+
+// Alignment
+
+// Background
+
+/*
+ Personality Traits
+ - personality trait
+ - flaws
+ - bonds
+ - ideals
+*/
+
+/*
+ Appearance
+  - description
+  - age
+  - height
+  - weight
+  - eyes
+  - hair
+  - scars
+  - distinguishing marks
+*/
+
+/*
+ Stats
+ - ability scores - https://dmingwithcharisma.com/2011/10/dd-stats-in-simple-language/
+ - ability modifiers?
+*/
+
+/*
+Proficiencies
+  - weapons
+  - armour
+  - tools
+  - other
+*/
+
+// Languages
+
+// Health
+
+// AC
+
+// Initiative
+
+// Speed
